70101431828d700afed5cf24a34816d0f6a0582f
wfgelvesp <wfgelves.91@gmail.com>
2022-02-22 14:21:06 -0500

    front-wfgelves

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -14,6 +14,8 @@ import { RegisterUserComponent } from './user/register-user/register-user.compon
 import { ListUsersComponent } from './user/list-users/list-users.component';
 import { UpdateUserComponent } from './user/update-user/update-user.component';
 import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field'
 import {ServiceService} from'./services/service.service';
 import { UserService } from './services/user.service';
 import { TokenInterceptorService } from './services/token-interceptor.service';
@@ -57,7 +59,9 @@ import {MatListModule} from '@angular/material/list';
     MatIconModule,
     MatSidenavModule,
     MatToolbarModule,
-    MatListModule
+    MatListModule,
+    MatCardModule,
+    MatFormFieldModule,
   ],
   providers: [ServiceService,UserService,TokenInterceptorService,AuthGuard],
   bootstrap: [AppComponent]
--- frontend/src/app/user/list-users/list-users.component.ts
+++ frontend/src/app/user/list-users/list-users.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { ActivatedRoute, Router, RouteReuseStrategy } from '@angular/router';
+import { Service } from 'src/app/interfaces/servicio';
+
+import { ServiceService } from 'src/app/services/service.service';
+import { UserService } from 'src/app/services/user.service';
+
 
 @Component({
   selector: 'app-list-users',
@@ -6,10 +14,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-users.component.css']
 })
 export class ListUsersComponent implements OnInit {
+  displayedColumns: string[] = ['Rol', 'Nombre', 'Email', 'Estado'];
+  dataSource = new MatTableDataSource<any>();
+  usersData:any;
 
-  constructor() { }
+  constructor(
+    private _userService:UserService,
+    private _router: Router,
+    private _Arouter : ActivatedRoute
+  ) { 
+    this.usersData={};
+    this.dataSource=new MatTableDataSource(this.usersData);
+  }
 
   ngOnInit(): void {
+    this._userService.listUsers().subscribe({
+      next: (v) => {
+        this.usersData = v.users;
+        console.log(v);
+        
+        // this.isPo = v.verifyPo;
+        this.dataSource = new MatTableDataSource(this.usersData);
+        //this.dataSource.paginator = this.paginator;
+      },
+      error: (e) => {
+        //this.message = e.error.message;
+        //this.openSnackBarError();
+      },
+    });  
   }
 
 }
