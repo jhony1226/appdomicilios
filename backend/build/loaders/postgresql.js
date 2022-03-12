469040624f682b09026084677a0c0502fac812e3
“jhony1226” <jhony1226@gmail.com>
2022-03-12 16:32:42 -0500

    funciona servidor  npm start

--- backend/build/loaders/postgresql.js
+++ backend/build/loaders/postgresql.js
@@ -8,6 +8,26 @@ const pg_1 = require("pg");
 //   user: process.env.USER,
 //   password: process.env.PASSWORD,
 // });
+/*
+const db = new Pool({
+  host: 'ec2-3-230-238-86.compute-1.amazonaws.com',
+  port: 5432,
+  database: 'd9dmtfaqasu48s',
+  user: 'lipzvdhtieiiim',
+  password: '89321d78d7d0dfa64f7bc11498413f74bccb1b3c18b436a1dc66919ffe65bdc6',
+  ssl: {
+     rejectUnauthorized: false
+     }
+});
+/*
+const db = new Pool({
+  host: 'localhost',
+  port: 5432,
+  database: 'appdomicilio',
+  user: 'postgres',
+  password: '12345',
+});
+*/
 const db = new pg_1.Pool({
     host: 'ec2-3-230-238-86.compute-1.amazonaws.com',
     port: 5432,
@@ -18,14 +38,5 @@ const db = new pg_1.Pool({
         rejectUnauthorized: false
     }
 });
-/*
-const db = new Pool({
-  host: 'localhost',
-  port: 5432,
-  database: 'appdomicilio',
-  user: 'postgres',
-  password: 'admin',
-   
-});*/
 exports.default = db;
 //# sourceMappingURL=postgresql.js.map
