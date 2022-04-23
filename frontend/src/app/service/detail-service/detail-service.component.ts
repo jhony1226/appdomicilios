a1538fd8239477b3307427135f903d74d3861e6d
wfgelvesp <wfgelves.91@gmail.com>
2022-04-23 14:50:32 -0500

    detalle service

--- frontend/src/app/service/detail-service/detail-service.component.ts
+++ frontend/src/app/service/detail-service/detail-service.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ServiceService } from 'src/app/services/service.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-detail-service',
@@ -8,13 +9,17 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrls: ['./detail-service.component.css']
 })
 export class DetailServiceComponent implements OnInit {
-  serviceData:any={};
+  displayedColumns: string[] = ['Cliente', 'Origen', 'Destino', 'Precio','Observaciones'];
+  dataSource:any=new MatTableDataSource<any>();
+  servicesData:any={};
 
   constructor(
     private _router: Router,
     private _Arouter: ActivatedRoute,
     private _serviceService:ServiceService
   ) {
+    this.servicesData={};
+    this.dataSource=new MatTableDataSource(this.servicesData);
    }
 
   ngOnInit(): void {
@@ -23,8 +28,13 @@ export class DetailServiceComponent implements OnInit {
       this._serviceService.listServiceById(service).subscribe({
         next:(v)=>{   
           //this._router.navigate(['/home/list-users'])
-          this.serviceData=v.servicios[0];
-          console.log(this.serviceData);
+          this.servicesData=v.servicios;
+          console.log(this.servicesData[0]);
+          
+          this.dataSource = new MatTableDataSource(this.servicesData);
+          
+          console.log(this.dataSource);
+          
 
         },
         error:(e)=>{ 
